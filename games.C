362055a851523e49e9e3e84b5bc8d253d9f8bf8b
MetroidGit <alouis@etud.insa-toulouse.fr>
2016-04-11 18:01:13 +0200

    Update games.C

--- games.C
+++ games.C
@@ -7,7 +7,7 @@ Games::Games(){
 
 Games::~Games(){}
 
-int Games::setGame0(){ // interactions de base àrajouter
+int Games::setGame0(){ // interactions de base Ã rajouter
 	while(1){
 		Games::myScreen.write("'Bonjour~ !'", 0, 1);
 
@@ -25,14 +25,31 @@ int Games::setGame1(){ // nourrir
 	if(A.read()){
 		Games::myScreen.write("'Merci !!'", 0, 0); 
 	} else if(B.read()){
-		Games::myScreen.write("'M�chant !'", 0, 0); 
+		Games::myScreen.write("'MÃchant !'", 0, 0); 
 	} else if(C.read()){
 		Games::myScreen.write("'Hey ! (>_<)'", 0, 0); 
 	}
 	return (0); 
 }
 
-int Games::setGame2(){
+int Games::setGame2(){//question existencielle
+	Games::myScreen.write("Suis-je un super chat? ", 0, 0);
+	if(A.read()){
+		Games::myScreen.write("YOUHOUHOU!!'", 0, 0); 
+		for(int i=0;i<4;i++){
+			for(int j =0; j<120;j++){
+                                this->myScreen.write("SUPER FORME !!!,1,0")
+                                sleep(2);
+                                this->myScreen.setRGB(j,255-j,j+j)
+                                }
+		}
+	} else if(B.read()){
+		Games::myScreen.write("BOUHOUH !'", 0, 0); 
+		this->myScreen.setRGB(240,0,0)
+	} else if(C.read()){
+		Games::myScreen.write("LOL!! OK", 0, 0);
+		this->myScreen.setRGB(71,137,183)
+	}
         return (0);   
 }
 
